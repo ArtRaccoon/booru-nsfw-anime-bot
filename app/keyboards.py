5ac391eb9ebbdc5b20acc0ca2fd39de44d12a271
Raccoon Art <potehinvlad7@gmail.com>
2026-07-08 20:40:26 +0300

    Add minimal main menu

--- app/keyboards.py
+++ app/keyboards.py
@@ -9,3 +9,14 @@ def search_keyboard() -> InlineKeyboardMarkup:
             [InlineKeyboardButton(text="🔎 Отправиться на поиски", callback_data="search:start")]
         ]
     )
+
+
+def main_menu_keyboard() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="🎲 Рандомный арт", callback_data="menu:random")],
+            [InlineKeyboardButton(text="⭐ Избранное", callback_data="menu:favorites")],
+            [InlineKeyboardButton(text="🔎 Поиск", callback_data="menu:search")],
+            [InlineKeyboardButton(text="💎 Премиум", callback_data="menu:premium")],
+        ]
+    )
